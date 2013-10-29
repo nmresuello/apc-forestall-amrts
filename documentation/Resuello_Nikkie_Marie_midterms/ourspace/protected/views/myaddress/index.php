ade4672db7cdfa3cd94bda0fa935a4d46bd07752
Nikkie Resuello <nmresuello@gmail.com>
2013-10-29 14:40:19 +0800

    I generated model and crud for table myaddress

---
+++ documentation/Resuello_Nikkie_Marie_midterms/ourspace/protected/views/myaddress/index.php
@@ -0,0 +1,20 @@
+<?php
+/* @var $this MyaddressController */
+/* @var $dataProvider CActiveDataProvider */
+
+$this->breadcrumbs=array(
+	'Myaddresses',
+);
+
+$this->menu=array(
+	array('label'=>'Create Myaddress', 'url'=>array('create')),
+	array('label'=>'Manage Myaddress', 'url'=>array('admin')),
+);
+?>
+
+<h1>Myaddresses</h1>
+
+<?php $this->widget('zii.widgets.CListView', array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_view',
+)); ?>
