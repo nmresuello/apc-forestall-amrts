74220116680ebf7b742325c28a339bdfbeb184ce
Marion Chloe Cambay <mcambayy@gmail.com>
2013-10-29 15:00:03 +0800

    configure gii and generate model and crud code

---
+++ documentation/cambay_marion_chloe_midterms/ourspace/protected/views/myaddress/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this MyaddressController */
+/* @var $data Myaddress */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('firstname')); ?>:</b>
+	<?php echo CHtml::encode($data->firstname); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('middlename')); ?>:</b>
+	<?php echo CHtml::encode($data->middlename); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('lastname')); ?>:</b>
+	<?php echo CHtml::encode($data->lastname); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('gender')); ?>:</b>
+	<?php echo CHtml::encode($data->gender); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created_at')); ?>:</b>
+	<?php echo CHtml::encode($data->created_at); ?>
+	<br />
+
+
+</div>
